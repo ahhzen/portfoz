e8d6dfcc56e5fd52b2f2a7bda80c6644a847f3aa
ahhzen <ahhzen@yahoo.com>
2020-07-06 22:56:27 +0800

    html correction

--- server.py
+++ server.py
@@ -1,78 +1,72 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pdb
+import csv
+import sys
+import os
+import pymongo
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/test")
-def hello():
-    return "Hello World"
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/index.html")
-def index2():
-    return render_template("index.html")
+@app.route("/<string:page_name>")
+def route_page(page_name, message=""):
+    return render_template(page_name, anymessage = message)
 
-@app.route("/about.html")
-def about():
-    return render_template("about.html")
-
-@app.route("/contact.html")
-def contact1(message=None):
-    return render_template("contact.html", anymessage = "")
-
-##@app.route("/contact.html/<message>")
-##def contact(message=None):
-##    return render_template("contact.html", anymessage = message)
-
-@app.route("/works.html")
-def works():
-    return render_template("works.html")
-
-@app.route("/work.html")
-def work():
-    return render_template("work.html")
-
-@app.route("/components.html")
-def components():
-    return render_template("components.html")
 
 @app.route("/submitcontact", methods=["POST", "GET"])
 def submitcontact():
     if request.method == "POST":
         try:
-            email = request.form.get("email", "No email")
-            subject = request.form.get("subject", "No Subject")
-            message = request.form.get("message", "No Message")
-            insertedid = store_contact(email, subject, message)
+            # insertedid = store_contact(data)
+            contactdata = request.form.to_dict()
+            insertedid = store_contact_csv(contactdata)
 
-            if insertedid:
-                message = "Success!"
+            if insertedid and type(insertedid) == bool:
+                message = "Thank You!"
                 return render_template("contact.html", anymessage=message)
-##                return redirect(url_for("contact", message="Thank you for your message"))
             else:
-                message = "Failed!"
+                message = insertedid
                 return render_template("contact.html", anymessage=message)
-##                return redirect(url_for("contact", message="Something went wrong. Please try again later"))
-
+        
         except:
+            message = insertedid
             print("Error when retrieving data from request")
-
-
-    return "Done"
-
-import pymongo
-def store_contact(email, subject, message):
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client["portfolio"]
-    collection = db["contact"]
-
-    return collection.insert_one({"email": email, "subject": subject, "message": message})
+            return render_template("contact.html", anymessage = message)
 
 
 
+def store_contact(data):
+    try:
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        db = client["portfolio"]
+        collection = db["contact"]
+
+        result = collection.insert_one({"name": data.get("name"), "email": data.get("email"), "subject": data.get("subject"), "message": data.get("message")})
+    except:
+        return False
+    else:
+        return True
+
+
+def store_contact_csv(data):
+    try:
+        with open("database/contact.csv", "a", newline="") as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
+
+            timestamp = datetime.now().strftime("%d/%m/%Y-%X")
+            csv_writer.writerow([data.get("name"), data.get("email"), data.get("subject"), data.get("message"), timestamp])
+            csvfile.close()
+    except:
+        err = sys.exc_info()[0]
+        message = f"Error: {err}"
+        return message
+    else:
+        return True
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
